e1b64ff8d27fda8301d30d09c7f917a292d03f1e
brigarcia13 <britney.garcia.arroyo@utelvt.edu.ec>
2022-03-24 09:31:37 -0500

    codigo de mi programa suma

---
+++ MoreiraJavier/suma.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+        float  x,y,z;
+        cout<<"ingrese numero x="; cin>> x;
+        cout<<"ingrese segundo numero y="; cin>> y;
+        z=x+y;
+        cout<<"El resultado de la suma es =";
+        cout<<z<<endl;
+        return(0);
+
+
+}
